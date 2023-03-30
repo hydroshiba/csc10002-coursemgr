a6fc4d57e722c54c819ad25d0573d8fa87d1516d
hydroshiba <36961737+hydroshiba@users.noreply.github.com>
2023-03-31 00:48:06 +0700

    Implemented hover & hold press coloring for button

--- Graphics/Button.cpp
+++ Graphics/Button.cpp
@@ -1,17 +1,28 @@
 #include "raylib.h"
 #include "Button.h"
 
-Button::Button(Vector2 pos, float width, float height, Color color) {
+Button::Button(Vector2 pos, float width, float height, Color fill, Color border, Color hover, Color press) {
     this->pos = pos;
+
     this->width = width;
     this->height = height;
-
     bounds = Rectangle{pos.x, pos.y, width, height};
-    this->color = color;
+
+    this->fill = fill;
+    this->border = border;
+    this->hover = hover;
+    this->press = press;
 }
 
-void Button::display() const {
-    DrawRectangle(pos.x, pos.y, width, height, color);
+void Button::display(const Vector2 &mouse) const {
+    DrawRectangle(pos.x, pos.y, width, height, border);
+
+    if(hovering(mouse))
+        DrawRectangle(pos.x + wborder, pos.y + wborder, width - wborder * 2, height - wborder * 2, hover);
+    else if(pressed(mouse))
+        DrawRectangle(pos.x + wborder, pos.y + wborder, width - wborder * 2, height - wborder * 2, press);
+    else
+        DrawRectangle(pos.x + wborder, pos.y + wborder, width - wborder * 2, height - wborder * 2, fill);
 }
 
 void Button::centerX() {
@@ -22,7 +33,17 @@ void Button::centerY() {
     pos.y = (GetScreenHeight() - height) / 2.0f;
 }
 
-bool Button::clicked(const Vector2 &mouse) {
+bool Button::clicked(const Vector2 &mouse) const {
     if(!CheckCollisionPointRec(mouse, bounds)) return false;
     return IsMouseButtonReleased(MOUSE_BUTTON_LEFT);
+}
+
+bool Button::pressed(const Vector2 &mouse) const {
+    if(!CheckCollisionPointRec(mouse, bounds)) return false;
+    return IsMouseButtonDown(MOUSE_BUTTON_LEFT);
+}
+
+bool Button::hovering(const Vector2 &mouse) const {
+    if(!CheckCollisionPointRec(mouse, bounds)) return false;
+    return IsMouseButtonUp(MOUSE_BUTTON_LEFT);
 }
--- Graphics/Button.h
+++ Graphics/Button.h
@@ -3,25 +3,43 @@
 
 #include "raylib.h"
 
+namespace button_color {
+
+const Color fill = SKYBLUE;
+const Color border = GRAY;
+
+const Color hover = BLUE;
+const Color clicked = DARKBLUE;
+
+}
+
+const float wborder = 2;
+
 class Button {
 private:
-    Rectangle bounds;
-    bool pressed;
+    Vector2 pos = Vector2{0, 0};
+    float width = 20, height = 10;
+
+    Color fill = button_color::fill, border = button_color::border;
+    Color hover = button_color::hover, press = button_color::clicked;
+
+    Rectangle bounds = Rectangle{pos.x, pos.y, width, height};
 
 public:
-    Vector2 pos;
-    float width, height;
-    
-    Color color;
-
     Button() {}
-    Button(Vector2 pos, float width, float height, Color color);
+    Button(Vector2 pos, float width, float height,
+            Color fill = button_color::fill,
+            Color border = button_color::border,
+            Color hover = button_color::hover,
+            Color press = button_color::clicked);
 
-    void display() const;
+    void display(const Vector2 &mouse) const;
     void centerX();
     void centerY();
 
-    bool clicked(const Vector2 &mouse);
+    bool clicked(const Vector2 &mouse) const;
+    bool pressed(const Vector2 &mouse) const;
+    bool hovering(const Vector2 &mouse) const;
 };
 
 #endif //BUTTON_H
