4ed2a9eaf813bb4648d9a98b21faedb5600631ac
hydroshiba <36961737+hydroshiba@users.noreply.github.com>
2023-04-04 10:04:27 +0700

    Reduced font loading size

--- Graphics/Text.h
+++ Graphics/Text.h
@@ -15,8 +15,8 @@ struct Text {
 	Color color;
 
 	Text();
-	Text(const char* text, float fsize = text_const::size, Font font = LoadFontEx(text_const::font_path, 256, 0, 0), float space = text_const::space, Color color = text_const::color);
-	Text(std::string text, float fsize = text_const::size, Font font = LoadFontEx(text_const::font_path, 256, 0, 0), float space = text_const::space, Color color = text_const::color);
+	Text(const char* text, float fsize = text_const::size, Font font = LoadFontEx(text_const::font_path, 192, 0, 0), float space = text_const::space, Color color = text_const::color);
+	Text(std::string text, float fsize = text_const::size, Font font = LoadFontEx(text_const::font_path, 192, 0, 0), float space = text_const::space, Color color = text_const::color);
 
 	void operator=(std::string text);
 	void operator=(const char* text);
