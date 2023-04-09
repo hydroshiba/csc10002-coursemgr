979ecdde80b0cc51de0d52eb97da8d480b84d5cb
HuyZerox <huyzerox@gmail.com>
2023-04-09 19:37:53 +0700

    Add some overload function to get object

--- Components/Components.h
+++ Components/Components.h
@@ -57,7 +57,11 @@ Student* getStudent(Vector<SchoolYear>& schoolYears, const std::string& studentI
 // Find staff
 Staff* getStaff(Vector<Staff> staffs, const std::string& staffID);
 // Find SchoolYear
+SchoolYear* getSchoolYear(Vector<SchoolYear>& schoolYears, const std::string& start);
+// Find SchoolYear
 SchoolYear* getSchoolYear(Vector<SchoolYear>& schoolYears, const unsigned int& start);
+// Find AcademicYear
+AcademicYear* getAcademicYear(Vector<AcademicYear>& academicYears, const std::string& start);
 // Find AcademicYear
 AcademicYear* getAcademicYear(Vector<AcademicYear>& academicYears, const unsigned int& start);
 // Find Class
