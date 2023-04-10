b3301b4f92063a59be401810433f6f0edd37510c
HuyZerox <huyzerox@gmail.com>
2023-04-10 19:44:47 +0700

    Search function: Successfully split search functional group to header file SearchFunction.h

--- Components/Components.h
+++ Components/Components.h
@@ -43,7 +43,7 @@ struct Scoreboard;
 //#include "Course.h"
 //#include "Scoreboard.h"
 
-#include "ConvertType.h"
+
 
 /* Forward declaration of struct operator overloading */
 
@@ -52,47 +52,6 @@ bool operator!=(const Type &typeA, const Type &typeB) {
     return !(typeA == typeB);
 }
 
-/*		Search function		*/
-// Find student
-Student* getStudent(Vector<SchoolYear>& schoolYears, const std::string& studentID);
-// Find staff
-Staff* getStaff(Vector<Staff> staffs, const std::string& staffID);
-// Find SchoolYear
-SchoolYear* getSchoolYear(Vector<SchoolYear>& schoolYears, const std::string& start);
-// Find SchoolYear
-SchoolYear* getSchoolYear(Vector<SchoolYear>& schoolYears, const unsigned int& start);
-// Find AcademicYear
-AcademicYear* getAcademicYear(Vector<AcademicYear>& academicYears, const std::string& start);
-// Find AcademicYear
-AcademicYear* getAcademicYear(Vector<AcademicYear>& academicYears, const unsigned int& start);
-// Find Class
-Class* getClass(Vector<SchoolYear>& schoolYears, const std::string& className);
-// Find Semester
-Semester* getSemester(Vector<AcademicYear>& academicYears, const std::string& semesterID);
-// Find Course
-Course* getCourse(Vector<AcademicYear>& academicYears, const std::string& courseID);
-// Find Scoreboard of Course of Student
-Scoreboard* getScoreboard(Vector<SchoolYear>& schoolYears, Vector<AcademicYear>& academicYears, const std::string& courseID, const std::string& studentID);
-// Check student acc
-bool isCorrectStaffAccount(Vector<Staff> staffs, const std::string& staffID, const std::string& password);
-// Check staff acc
-bool isCorrectStudentAccount(Vector<SchoolYear>& schoolYears, const std::string& studentID, const std::string& password);
-// Check student
-bool isStudent(Vector<SchoolYear>& schoolYears, const std::string& studentID, std::string& outStr);
-// Check staff
-bool isStaff(Vector<Staff> staffs, const std::string& staffID, std::string& outStr);
-// Check SchoolYear
-bool isSchoolYear(Vector<SchoolYear>& schoolYears, const std::string& start, std::string& outStr);
-// Check AcademicYear
-bool isAcademicYear(Vector<AcademicYear>& academicYears, const std::string& start, std::string& outStr);
-// Check Class
-bool isClass(Vector<SchoolYear>& schoolYears, const std::string& className, std::string& outStr);
-// Check Semester
-bool isSemester(Vector<AcademicYear>& academicYears, const std::string& semesterID, std::string& outStr);
-// Check Course
-bool isCourse(Vector<AcademicYear>& academicYears, const std::string& courseID, std::string& outStr);
-
-//----------------------------------------------------------------------------------------------//
 
 /*		Insert function		*/
 
---
+++ Components/SearchFunction.h
@@ -0,0 +1,48 @@
+#ifndef SEARCH_FUNCTION_H
+#define SEARCH_FUNCTION_H
+
+#include "Components.h"
+
+/*		Search function		*/
+// Find student
+Student* getStudent(Vector<SchoolYear>& schoolYears, const std::string& studentID);
+// Find staff
+Staff* getStaff(Vector<Staff> staffs, const std::string& staffID);
+// Find SchoolYear
+SchoolYear* getSchoolYear(Vector<SchoolYear>& schoolYears, const std::string& start);
+// Find SchoolYear
+SchoolYear* getSchoolYear(Vector<SchoolYear>& schoolYears, const unsigned int& start);
+// Find AcademicYear
+AcademicYear* getAcademicYear(Vector<AcademicYear>& academicYears, const std::string& start);
+// Find AcademicYear
+AcademicYear* getAcademicYear(Vector<AcademicYear>& academicYears, const unsigned int& start);
+// Find Class
+Class* getClass(Vector<SchoolYear>& schoolYears, const std::string& className);
+// Find Semester
+Semester* getSemester(Vector<AcademicYear>& academicYears, const std::string& semesterID);
+// Find Course
+Course* getCourse(Vector<AcademicYear>& academicYears, const std::string& courseID);
+// Find Scoreboard of Course of Student
+Scoreboard* getScoreboard(Vector<SchoolYear>& schoolYears, Vector<AcademicYear>& academicYears, const std::string& courseID, const std::string& studentID);
+// Check student acc
+bool isCorrectStaffAccount(Vector<Staff> staffs, const std::string& staffID, const std::string& password);
+// Check staff acc
+bool isCorrectStudentAccount(Vector<SchoolYear>& schoolYears, const std::string& studentID, const std::string& password);
+// Check student
+bool isStudent(Vector<SchoolYear>& schoolYears, const std::string& studentID, std::string& outStr);
+// Check staff
+bool isStaff(Vector<Staff> staffs, const std::string& staffID, std::string& outStr);
+// Check SchoolYear
+bool isSchoolYear(Vector<SchoolYear>& schoolYears, const std::string& start, std::string& outStr);
+// Check AcademicYear
+bool isAcademicYear(Vector<AcademicYear>& academicYears, const std::string& start, std::string& outStr);
+// Check Class
+bool isClass(Vector<SchoolYear>& schoolYears, const std::string& className, std::string& outStr);
+// Check Semester
+bool isSemester(Vector<AcademicYear>& academicYears, const std::string& semesterID, std::string& outStr);
+// Check Course
+bool isCourse(Vector<AcademicYear>& academicYears, const std::string& courseID, std::string& outStr);
+
+//----------------------------------------------------------------------------------------------//
+
+#endif
